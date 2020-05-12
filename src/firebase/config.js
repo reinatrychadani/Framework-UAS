730d25f0c6995b67457cc38cb4defb3a2a425de6
reinatrychadani <trycha67@gmail.com>
2020-05-12 15:33:47 +0700

    Menambahkan class config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAfogjzj-oLFp4e9Gzya3RWb1A4RIQMj4E",
+    authDomain: "framework-uas.firebaseapp.com",
+    databaseURL: "https://framework-uas.firebaseio.com",
+    projectId: "framework-uas",
+    storageBucket: "framework-uas.appspot.com",
+    messagingSenderId: "478592957166",
+    appId: "1:478592957166:web:e7860f6c3a6b4631ab11ff",
+    measurementId: "G-GZVRQCTERM"
+  };
+
+  export default firebaseConfig;
