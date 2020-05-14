60e1cf74c990b416cf1c5a38ea03f71478ee927d
reinatrychadani <trycha67@gmail.com>
2020-05-14 09:49:53 +0700

    Mengedit class index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import Main from "../components/Main";
 import Buku from "./components/Buku";
 import "./style.css";
 
 ReactDOM.render (
-    //<Main />,
     <Buku />,
 	document.getElementById("root")
 );
